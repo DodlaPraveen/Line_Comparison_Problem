2c8e1aa0c238b9fc4ac4e07c2f11177c7afb0858
praveen dodla <dodlapraveen09@gmail.com>
2022-08-03 15:54:38 +0530

    Using Compare to method To check the eqality of the line

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -4,32 +4,46 @@ namespace LineComparison
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void CompareTo()//method to which line is greater
         {
-
-            Console.WriteLine("Welcome To Line Comparison Computation.");
-            int x1, x2, y1, y2, x3, y3, x4, y4;
-            Console.WriteLine("Enter values x1 and y1 for Line 1");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x2 and y2 for Line 1");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x1 and y1 for Line 2");
-            x3 = Convert.ToInt32(Console.ReadLine());
-            y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x2 and y2 for Line 2");
-            x4 = Convert.ToInt32(Console.ReadLine());
-            y4 = Convert.ToInt32(Console.ReadLine());
-            double len1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            double len2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            int diffInLength = len1.CompareTo(len2);
-            if (diffInLength == 0)
-                Console.WriteLine("Lines are equal in length");
-            else if (diffInLength > 0)
-                Console.WriteLine("Line1 is greater in length than Line2");
+            Console.WriteLine("Please Enter the Values of X1,X2,Y1,Y2 of the First Line");
+            double x1 = Console.Read();//User input Length
+            double x2 = Console.Read();
+            double y1 = Console.Read();
+            double y2 = Console.Read();
+            double LengthofX = (x2 - x1);//based on the formula for first line
+            double LengthofY = (y2 - y1);//based on the formula for first line
+            double PowX = Math.Pow(LengthofX, 2);//calculting the power (X2-X1)^2. for first line
+            double PowY = Math.Pow(LengthofY, 2);//calculting the power (Y2-Y1)^2. for first line
+            double LengthSq = PowX + PowY;//(X2-X1)^2 - (Y2-Y1)^2. for first line
+            double LengthofLine = Math.Sqrt(LengthSq);//calculating sqare root. for first line
+            Console.WriteLine("Lengthof Line" + LengthofLine);
+            Console.WriteLine("Please Enter the Values of X1,X2,Y1,Y2 of the Second Line");
+            double x12 = Console.Read();
+            double x22 = Console.Read();
+            double y12 = Console.Read();
+            double y22 = Console.Read();
+            double LengthofX2 = (x22 - x12);//based on the formula for second line
+            double LengthofY2 = (y22 - y12);//based on the formula for second line
+            double PowX2 = Math.Pow(LengthofX2, 2);//calculting the power (X2-X1)^2. for second line
+            double PowY2 = Math.Pow(LengthofY2, 2);//calculting the power (X2-X1)^2. for second line
+            double LengthSq2 = PowX2 + PowY2;//(X2-X1)^2 - (Y2-Y1)^2. for second line
+            double LengthofLine2 = Math.Sqrt(LengthSq2);//calculating sqare root. for second line
+            Console.WriteLine("Lengthof Line" + LengthofLine2);
+            if (LengthofLine < LengthofLine2)
+            {
+                Console.WriteLine("First Line is Smaller");
+            }
+            else if (LengthofLine > LengthofLine2)
+            {
+                Console.WriteLine("Second Line is Smaller");
+            }
             else
-                Console.WriteLine("Line1 is smaller in length than Line2");
+                Console.WriteLine("Both the Lines are Equal");
+        }
+        static void Main(string[] args)//main method
+        {
+            Program.CompareTo();//creating object of the class.
         }
     }
 }
